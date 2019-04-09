6f5a4cf60b4e85d4b4a4864d981da9112de7141e
RUSya82 <mail-spam2012@yandex.ru>
2019-04-09 20:29:37 +0500

    вывел результаты работы компонента test через вьюху

--- views/test/index.php
+++ views/test/index.php
@@ -1,9 +1,11 @@
 <?php
 /* @var $product \app\models\Product */
 /* @var $this yii\web\View */
+/* @var $test string*/
 use yii\web\View;
 echo "This is file 'test/index.php'<br>";
 
-echo $product->name;
-echo \yii\widgets\DetailView::widget(['model' => $product]);
+//echo $product->name;
+//echo \yii\widgets\DetailView::widget(['model' => $product]);
+echo $test;
 
