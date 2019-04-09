0a8aedd9f2aa2a918c87b942694c17b93754a8dd
RUSya82 <mail-spam2012@yandex.ru>
2019-04-09 14:31:30 +0500

    Homework 2 done

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -16,9 +16,9 @@ class TestController extends Controller
 
     public function actionIndex()
     {
-        return Yii::$app->test->run();
-        //$product = new Product([ 'id'=> 35, 'name'=>'reebok', 'category' => 'shoose', 'price' => 350]);
-        //return $this->render('index', ['product' => $product]);
+        //return Yii::$app->test->run();
+        $product = new Product([ 'id'=> 35, 'name'=>'reebok', 'category' => 'shoose', 'price' => 350]);
+        return $this->render('index', ['product' => $product]);
         //return $this->render('index');
         //return $this->renderContent('It is class TestController');
     }
