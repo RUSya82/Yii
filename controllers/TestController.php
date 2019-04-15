0904e7f55f3a33b4522f662786b1e01c5b118f16
RUSya82 <mail-spam2012@yandex.ru>
2019-04-15 17:12:26 +0500

    Сделал Insert  в таблицу user

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -18,7 +18,26 @@ class TestController extends Controller
     {
         //$test = Yii::$app->test->run();
         //$product = new Product([ 'id'=> 35, 'name'=>'reebok', 'category' => 'shoose', 'price' => 350]);
-        $data = \Yii::$app->db->createCommand('SELECT [[name]], [[price]], [[category]] FROM {{product}}')->queryAll();
+        //$data = \Yii::$app->db->createCommand('SELECT [[name]], [[price]], [[category]] FROM {{product}}')->queryAll();
+//        $data = \Yii::$app->db->createCommand()->insert('user', [
+//            'username' => 'Anton',
+//            'password_hash' => '2df68sd45f6df8g2q3lp',
+//            'creator_id' => 35,
+//            'created_at' =>time(),
+//        ])->execute();
+        $data = \Yii::$app->db->createCommand()->insert('user', [
+            'username' => 'Marat',
+            'password_hash' => '65d689d5rd1df8g2q3lp',
+            'creator_id' => 35,
+            'created_at' =>time(),
+        ])->execute();
+        $data = \Yii::$app->db->createCommand()->insert('user', [
+            'username' => 'Ubuntu',
+            'password_hash' => '86g689d5rdf5f8g2q5tg',
+            'creator_id' => 35,
+            'created_at' =>time(),
+        ])->execute();
+        //var_dump($data);
         return $this->render('index', ['test' => $data]);
         //return $this->render('index');
         //return $this->renderContent('It is class TestController');
